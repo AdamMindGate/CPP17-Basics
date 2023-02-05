9bdaac0cfc043c028a2ed27f8016a7ec1dbb528e
AdamMindGate <dirtyhovac@gmail.com>
2023-02-05 19:46:53 +0200

    add medium challenges

---
+++ Section 6 - Selection/Homework (Medium Challenges)/HW1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+//AdamMindGate
+
+//Problem #1: Find Maximum of 10
+int main()
+{
+    int num,  biggest = 0;
+
+    cin>>num; if(num>biggest){biggest = num;}
+    cin>>num; if(num>biggest){biggest = num;}
+    cin>>num; if(num>biggest){biggest = num;}
+    cin>>num; if(num>biggest){biggest = num;}
+    cin>>num; if(num>biggest){biggest = num;}
+    cin>>num; if(num>biggest){biggest = num;}
+    cin>>num; if(num>biggest){biggest = num;}
+    cin>>num; if(num>biggest){biggest = num;}
+    cin>>num; if(num>biggest){biggest = num;}
+    cin>>num; if(num>biggest){biggest = num;}
+
+    cout<<biggest;
+
+    return 0;
+
+}
+
---
+++ Section 6 - Selection/Homework (Medium Challenges)/HW2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+//AdamMindGate
+
+//Problem #2: Find Maximum up to 10
+int main()
+{
+    int n,numbs,biggest =0;
+    cin>>n;
+    if (n>0){cin>>numbs; n-=1;
+    if(numbs>biggest)
+    biggest = numbs;}
+
+    if (n>0){cin>>numbs; n-=1;
+    if(numbs>biggest)
+    biggest = numbs;}
+
+    if (n>0){cin>>numbs; n-=1;
+    if(numbs>biggest)
+    biggest = numbs;}
+
+    if (n>0){cin>>numbs; n-=1;
+    if(numbs>biggest)
+    biggest = numbs;}
+
+    if (n>0){cin>>numbs; n-=1;
+    if(numbs>biggest)
+    biggest = numbs;}
+
+    if (n>0){cin>>numbs; n-=1;
+    if(numbs>biggest)
+    biggest = numbs;}
+
+    if (n>0){cin>>numbs; n-=1;
+    if(numbs>biggest)
+    biggest = numbs;}
+
+    if (n>0){cin>>numbs; n-=1;
+    if(numbs>biggest)
+    biggest = numbs;}
+
+    if (n>0){cin>>numbs; n-=1;
+    if(numbs>biggest)
+    biggest = numbs;}
+
+    if (n>0){cin>>numbs; n-=1;
+    if(numbs>biggest)
+    biggest = numbs;}
+
+    cout<<biggest;
+    return 0;
+}
+
